54144de4f4f7070f8797bb2a0657b1c116e6e1a9
titantooth <ali.rabieeie@gmail.com>
2023-08-12 20:47:05 +0330

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import NavBar from './components/navbar/navbar.tsx'
+import Banner from './components/hero/banner.tsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div>
       <NavBar />
+      <Banner />
     </div>
   )
 }
