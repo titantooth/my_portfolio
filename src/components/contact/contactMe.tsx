de6d8962f3c8c395c8080cc276c03d31fde6b0b8
titantooth <ali.rabieeie@gmail.com>
2023-08-13 13:33:53 +0330

    some work on the contact form and handler

--- src/components/contact/contactMe.tsx
+++ src/components/contact/contactMe.tsx
@@ -25,8 +25,26 @@ const contactMe = () => {
         })
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setButtonText('Sending...')
+        let response = await fetch("http://localhost:5000/contact", {
+            method: 'POST',
+            headers: {
+                'Content-Type': "Application/json;charset=utf-8"
 
+            },
+            body: JSON.stringify(formDetails),
+
+        })
+        setButtonText('Send')
+        let result = response.json()
+        setFormDetails(formInitialDetails)
+        if (result.code === 200) {
+            setStatus({ success: true, message: "Message Sent Successfully!" })
+        } else {
+            setStatus({ success: false, message: 'Something went wrong, please try again later.' })
+        }
     }
 
     return (
