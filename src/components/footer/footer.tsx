c86759fdd88b995fb08127489bb53f0b58174891
titantooth <ali.rabieeie@gmail.com>
2023-09-17 15:14:52 +0330

    fixed bug in footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,5 @@
 import './footer.css'
-import logo from '../../assets/img/logo.svg'
+// import logo from '../../assets/img/logo.svg'
 import navIcon1 from '../../assets/img/nav-icon1.svg'
 import navIcon2 from '../../assets/img/nav-icon2.svg'
 import navIcon3 from '../../assets/img/nav-icon3.svg'
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,7 +1,6 @@
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./projectCard.jsx";
 // import projImg1 from "../../assets/img/project-img1.png";
-import projImg2 from "../../assets/img/project-img2.png";
 import projImg3 from "../../assets/img/project-img3.png";
 import colorSharp2 from "../../assets/img/color-sharp2.png";
 import chitgar from '../../assets/img/chitgar.png'
